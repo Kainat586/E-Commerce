a7348aad891a32cef6faa042ddba34d19cc4faf0
Kainat586 <kainat.khuram786@gmail.com>
2025-11-22 15:11:57 +0500

    Delete E commerce directory

--- E commerce/backend/src/styles/styles.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StylesService } from './styles.service';
-import { StylesController } from './styles.controller';
-
-@Module({
-  providers: [StylesService],
-  controllers: [StylesController]
-})
-export class StylesModule {}
